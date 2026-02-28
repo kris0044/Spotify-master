f163968d48e128df820ace7ae115a8ae694ecfce
kris0044 <161619183+kris0044@users.noreply.github.com>
2026-02-28 10:48:23 +0530

    Update baseURL for production environment

--- src/stores/useChatStore.ts
+++ src/stores/useChatStore.ts
@@ -22,7 +22,8 @@ interface ChatStore {
 	setSelectedUser: (user: User | null) => void;
 }
 
-const baseURL = import.meta.env.MODE === "development" ? "http://localhost:5000" : "/";
+const baseURL = import.meta.env.MODE === "development" ? "https://spotify-master-backend-9di9.onrender.com" : "/";
+// const baseURL = import.meta.env.MODE === "development" ? "http://localhost:5000" : "/";
 
 const socket = io(baseURL, {
 	autoConnect: false, // only connect if user is authenticated
