8af351a16716333210b5efa5d43ca95875b9e517
kris0044 <krismistry74@gmail.com>
2026-02-28 10:14:23 +0530

    BUILD UPDATE

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+	 build: {
+    outDir: 'build',  // Add this line
+	    chunkSizeWarningLimit: 1000, // increases limit to 1000kb
+
+  },
 	plugins: [react()],
 	resolve: {
 		alias: {
